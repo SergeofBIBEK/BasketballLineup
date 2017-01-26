61fa695a920eac939391bc992f4cfe50357aa8d8
SergeofBIBEK <SergeofBIBEK@gmail.com>
2017-01-25 22:01:55 -0500

    Create fbdbref.js

---
+++ fbdbref.js
@@ -0,0 +1,9 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA2RncOjv4KIM43y7qTlr79Pqg3GkeXyNQ",
+    authDomain: "basketball-lineup.firebaseapp.com",
+    databaseURL: "https://basketball-lineup.firebaseio.com",
+    storageBucket: "basketball-lineup.appspot.com",
+    messagingSenderId: "890404077686"
+  };
+  firebase.initializeApp(config);
