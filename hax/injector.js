4bb76af94ec28f9ad5bc64fc9e4637faee95ee14
bgates-mc <33226837+bgates-mc@users.noreply.github.com>
2018-05-09 22:49:00 -0400

    Update injector.js

--- hax/injector.js
+++ hax/injector.js
@@ -1,46 +1,59 @@
-var baseURL = 'http://lineupgenerator.ga/hax'
+var haxBaseURL = 'http://lineupgenerator.ga/hax'
 
 //append the css
 // <link href=/static/css/app.css rel=stylesheet>
 
-var haxCss = document.createElement('link');
-haxCss.href = `${baseURL}/static/css/app.css`;
-haxCss.rel = "stylesheet";
-document.head.appendChild(haxCss);
+function appendHaxCSS() {
+    var haxCss = document.createElement('link');
+    haxCss.href = `${baseURL}/static/css/app.css`;
+    haxCss.rel = "stylesheet";
+    haxCss.onload = appendHaxDiv;
+    document.head.appendChild(haxCss);
+}
 
 //append the div
 //<div id=hax>
 
-var haxRoot = document.createElement('div');
-haxRoot.id = "hax";
-document.body.appendChild(haxRoot);
+function appendHaxDiv() {
+    var haxRoot = document.createElement('div');
+    haxRoot.id = "hax";
+    haxRoot.onload = appendHaxManifest;
+    document.body.appendChild(haxRoot);
+}
 
 //append the js
 //><script type=text/javascript src=/static/js/manifest.js></script>
 
-var haxManifest = document.createElement('script');
-haxManifest.async = "false";
-haxManifest.type = 'text/javascript';
-haxManifest.src = `${baseURL}/static/js/manifest.js`;
-
-document.body.appendChild(haxManifest);
-
+function appendHaxManifest() {
+    var haxManifest = document.createElement('script');
+    haxManifest.type = 'text/javascript';
+    haxManifest.src = `${baseURL}/static/js/manifest.js`;
+    haxManifest.onload = appendHaxVendor;
+    document.body.appendChild(haxManifest);
+}
 //<script type=text/javascript src=/static/js/vendor.js></script>
 
-var haxVendor = document.createElement('script');
-haxVendor.async = "false";
-haxVendor.type = 'text/javascript';
-haxVendor.src = `${baseURL}/static/js/vendor.js`;
-
-document.body.appendChild(haxVendor);
+function appendHaxVendor() {
+    var haxVendor = document.createElement('script');
+    haxVendor.type = 'text/javascript';
+    haxVendor.src = `${baseURL}/static/js/vendor.js`;
+    haxVendor.onload = appendHaxAppScript;
+    document.body.appendChild(haxVendor);
+}
 
 //<script type=text/javascript src=/static/js/app.js></script>
 
-var haxAppScript = document.createElement('script');
-haxAppScript.async = "false";
-haxAppScript.type = 'text/javascript';
-haxAppScript.src = `${baseURL}/static/js/app.js`;
+function appendHaxAppScript() {
+    var haxAppScript = document.createElement('script');
+    haxAppScript.type = 'text/javascript';
+    haxAppScript.src = `${baseURL}/static/js/app.js`;
+    haxAppScript.onload = haxHideOriginal;
+    document.body.appendChild(haxAppScript);
 
-document.body.appendChild(haxAppScript);
+}
 
 //hide the original
+
+function haxHideOriginal() {
+    console.log('hide it all!');
+}
