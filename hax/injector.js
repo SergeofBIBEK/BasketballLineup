47c6a18d17cb4dc7a63a3e0953ca298a02247d34
bgates-mc <33226837+bgates-mc@users.noreply.github.com>
2018-05-09 22:12:33 -0400

    Create injector.js

---
+++ hax/injector.js
@@ -0,0 +1,43 @@
+let baseURL = 'http://lineupgenerator.ga/hax'
+
+//append the css
+// <link href=/static/css/app.css rel=stylesheet>
+
+var haxCss = document.createElement('link');
+haxCss.href = `${baseURL}/static/css/app.css`;
+haxCss.rel = "stylesheet";
+document.head.appendChild(haxCss);
+
+//append the div
+//<div id=hax>
+
+var haxRoot = document.createElement('div');
+haxRoot.id = "hax";
+document.body.appendChild(haxRoot);
+
+//append the js
+//><script type=text/javascript src=/static/js/manifest.js></script>
+
+var haxManifest = document.createElement('script');
+haxManifest.type = 'text/javascript';
+haxManifest.src = `${baseURL}/static/js/manifest.js`;
+
+document.body.appendChild(haxManifest);
+
+//<script type=text/javascript src=/static/js/vendor.js></script>
+
+var haxVendor = document.createElement('script');
+haxVendor.type = 'text/javascript';
+haxVendor.src = `${baseURL}/static/js/vendor.js`;
+
+document.body.appendChild(haxVendor);
+
+//<script type=text/javascript src=/static/js/app.js></script>
+
+var haxAppScript = document.createElement('script');
+haxAppScript.type = 'text/javascript';
+haxAppScript.src = `${baseURL}/static/js/app.js`;
+
+document.body.appendChild(haxAppScript);
+
+//hide the original
